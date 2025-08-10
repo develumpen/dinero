d2a0104606d86f738247360d720a4d0c5829ed18
develumpen <hi@develumpen.com>
2025-08-10 17:44:03 -0500

    crud accounts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_10_003905) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_10_214411) do
+  create_table "accounts", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
