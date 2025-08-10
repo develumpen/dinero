1673ad0922f648275eb6e4d4d589295a00fb3a4e
Develumpen <44843793+develumpen@users.noreply.github.com>
2025-08-09 20:12:02 -0500

    Import csv files (#1)
    * install active storage
    
    * basic csv import and list
    
    * add some import metadata
    
    * add basic menu
    
    * fix rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
+  resources :csv_imports, path: "imports"
+
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "csv_imports#index"
 end
